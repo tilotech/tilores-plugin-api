4b0764dd1108655b91786f8bed43c4c4abf08cf3
Stefan Berkner <stefan.berkner@tilotech.io>
2021-11-17 13:29:54 +0100

    Add proper readme

--- entity.go
+++ entity.go
@@ -1,5 +1,6 @@
 package api
 
+// Entity represents a real world object
 type Entity struct {
 	ID      string
 	Records []*Record
--- record.go
+++ record.go
@@ -1,5 +1,8 @@
 package api
 
+// Record represents a part of an Entity and the corresponding predicates
+//
+// Each Record must have a unique ID.
 type Record struct {
 	ID   string
 	Data map[string]interface{}
